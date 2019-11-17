87587ac83a1894eb12e5a6c126c504bd0717ac9f
TSUTSUI,Kazuo <kzt206@gmail.com>
2019-11-17 20:41:06 +0900

    Set FloodCell getter/setter

--- src/suiri2_3/FloodCell.java
+++ src/suiri2_3/FloodCell.java
@@ -1,5 +1,44 @@
 package suiri2_3;
 
 public class FloodCell {
-
+	private int x;
+	private int y;
+	private double zb;
+	private char ip;
+	private double rn;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public double getZb() {
+		return zb;
+	}
+	public void setZb(double zb) {
+		this.zb = zb;
+	}
+	public char getIp() {
+		return ip;
+	}
+	public void setIp(char ip) {
+		this.ip = ip;
+	}
+	public double getRn() {
+		return rn;
+	}
+	public void setRn(double rn) {
+		this.rn = rn;
+	}
+	
+	@Override
+	public String toString() {
+		return "x="+this.getX()+", y="+this.getY();
+	}
 }
