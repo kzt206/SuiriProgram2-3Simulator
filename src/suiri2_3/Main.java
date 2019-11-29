251bf68f779c6fa3f28f0620e71644a7ad098533
TSUTSUI,Kazuo <kzt206@gmail.com>
2019-11-29 22:56:18 +0900

    Add data block and set Break Point

--- src/suiri2_3/Main.java
+++ src/suiri2_3/Main.java
@@ -13,9 +13,26 @@ public class Main {
 		arrayFC = fr.getData();
 		
 //		System.out.println(arrayFC);
-		System.out.println("Main.java :"+arrayFC.get(12).get(17)); //get(j or y).get(i or x)
+		System.out.println("Main.java :"+arrayFC.get(10).get(18)); //get(j or y).get(i or x)
 		IMAX = arrayFC.get(0).size();
 		JMAX = arrayFC.size();
 		System.out.printf("Main.java :IMAX:%d, JMAX:%d\n", IMAX, JMAX);
+		
+		//Data block
+		double DX = 285.44;
+		double DY = 231.0;
+		double G = 9.8;
+		double EPS = 0.001;
+		double DT = 2.5;
+		int IBR = 29-1;
+		int JBR = 42-1;
+		int IBRD = -1;
+		int JBRD = 0;
+		
+		//Set Break Point
+		//Chage of IP for LEVEE-Break Point
+		arrayFC.get(JBR).get(IBR).setIp("B");
+		
+		
 	}
 }
